2eb2a077b93c00a20fd43891a7ddc0d583e42290
hizakurage <hizakurage@gmail.com>
2020-02-23 17:33:44 +0900

    emacs: Improve google-translate

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -55,7 +55,52 @@
   (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
 
 (el-get-bundle go-mode)
-(el-get-bundle google-translate)
+
+(el-get-bundle google-translate
+  (require 'google-translate)
+  ;; http://emacs.rubikitch.com/google-translate-sentence/
+  (defvar google-translate-english-chars "[:ascii:]’“”–"
+    "これらの文字が含まれているときは英語とみなす")
+  (defun google-translate-enja-or-jaen (&optional string)
+    "regionか、現在のセンテンスを言語自動判別でGoogle翻訳する。"
+    (interactive)
+    (setq string
+          (cond ((stringp string) string)
+                (current-prefix-arg
+                 (read-string "Google Translate: "))
+                ((use-region-p)
+                 (buffer-substring (region-beginning) (region-end)))
+                (t
+                 (save-excursion
+                   (let (s)
+                     (forward-char 1)
+                     (backward-sentence)
+                     (setq s (point))
+                     (forward-sentence)
+                     (buffer-substring s (point)))))))
+    (let* ((asciip (string-match
+                    (format "\\`[%s]+\\'" google-translate-english-chars)
+                    string)))
+      (run-at-time 0.1 nil 'deactivate-mark)
+      (google-translate-translate
+       (if asciip "en" "ja")
+       (if asciip "ja" "en")
+       string)))
+  (global-set-key (kbd "C-c t") 'google-translate-enja-or-jaen)
+
+  ;; Workaround
+  ;; https://qiita.com/akicho8/items/cae976cb3286f51e4632
+  ;; https://github.com/atykhonov/google-translate/issues/98
+  (defun google-translate-json-suggestion (json)
+    "Retrieve from JSON (which returns by the
+`google-translate-request' function) suggestion. This function
+does matter when translating misspelled word. So instead of
+translation it is possible to get suggestion."
+    (let ((info (aref json 7)))
+      (if (and info (> (length info) 0))
+          (aref info 1)
+        nil))))
+
 (el-get-bundle helm
   ;; https://qiita.com/jabberwocky0139/items/86df1d3108e147c69e2c
   (helm-mode 1)
