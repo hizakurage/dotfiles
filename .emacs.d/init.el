27e2115dc1b60123f0b98ca5bc3c2772690755ea
hizakurage <hizakurage@gmail.com>
2020-03-14 00:42:43 +0900

    emacs: Refactoring

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,8 +16,9 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; el-get
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
 
+;; Install el-get if not installed
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
       (url-retrieve-synchronously
